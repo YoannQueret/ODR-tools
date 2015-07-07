27a4a39102e65a6e4add2758835d5186a8ed4eba
Yoann QUEERET <yoann@yoann.queret.net>
2015-07-07 16:41:06 +0200

    Replace - with _ in instance to resolve compatibility with munin cgi-graph

--- munin/ODR_dabmux_stats.py
+++ munin/ODR_dabmux_stats.py
@@ -93,7 +93,7 @@ class App:
 		
 		munin_config = ""
 		for conf in config['config']:
-			munin_config += config_template.format(ident=conf)
+			munin_config += config_template.format(ident=conf.replace('-', '_'))
 		return munin_config
 		
 	def getValues(self):
@@ -103,13 +103,13 @@ class App:
 		munin_values = ""
 		for ident in values:
 			v = values[ident]['inputstat']
-			munin_values += "multigraph buffers_{ident}\n".format(ident=ident)
+			munin_values += "multigraph buffers_{ident}\n".format(ident=ident.replace('-', '_'))
 			munin_values += "high.value {}\n".format(v['max_fill'])
 			munin_values += "low.value {}\n".format(v['min_fill'])
-			munin_values += "multigraph over_underruns_{ident}\n".format(ident=ident)
+			munin_values += "multigraph over_underruns_{ident}\n".format(ident=ident.replace('-', '_'))
 			munin_values += "underruns.value {}\n".format(v['num_underruns'])
 			munin_values += "overruns.value {}\n".format(v['num_overruns'])
-			munin_values += "multigraph audio_levels_{ident}\n".format(ident=ident)
+			munin_values += "multigraph audio_levels_{ident}\n".format(ident=ident.replace('-', '_'))
 			munin_values += "left.value {}\n".format(v['peak_left'])
 			munin_values += "right.value {}\n".format(v['peak_right'])
 		return munin_values
