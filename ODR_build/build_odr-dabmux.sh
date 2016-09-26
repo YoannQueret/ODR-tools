5bfe1ca686673d6a0b3658918c4f749a53532e08
Yoann QUERET <yoann@queret.net>
2016-09-26 20:50:25 +0000

    Rename file and modifed dabmux and dabmod

--- ODR_build/build_odr-dabmux.sh
+++ ODR_build/build_odr-dabmux.sh
@@ -6,6 +6,7 @@ RUN_DIR=$(dirname $(readlink -f $0))
 
 DEB_VERSION="jessie"
 ARCH="amd64"
+PKG_NAME="odr-dabmux"
 
 mkdir -p ${BUILD_DIR}
 mkdir -p ${PKG_DIR}
@@ -14,16 +15,22 @@ mkdir -p ${PKG_DIR}
 cd ${BUILD_DIR}
 git clone https://github.com/Opendigitalradio/ODR-DabMux.git
 cd ODR-DabMux/
+
 VERSION=`git describe --tags`
 VERSION=${VERSION#v*}
+GIT_HASH=`git log --pretty=format:'%h' -n 1`
+PKG_VERSION="${VERSION}+${GIT_HASH}"
+DESTDIR="${PKG_DIR}/${PKG_NAME}-${PKG_VERSION}~${DEB_VERSION}_${ARCH}/"
+
 ./bootstrap.sh
 ./configure --enable-output-raw --enable-input-udp
 make
-make install DESTDIR=${PKG_DIR}/ODR-DabMux-${VERSION}~${DEB_VERSION}_${ARCH}/
+make install DESTDIR=${DESTDIR}
 
 
 
-cp -r ${RUN_DIR}/ODR-DabMux/DEBIAN/ "${PKG_DIR}/ODR-DabMux-${VERSION}~${DEB_VERSION}_${ARCH}/"
-sed "s/##VERSION##/${VERSION}/g" "${PKG_DIR}/ODR-DabMux-${VERSION}~${DEB_VERSION}_${ARCH}/DEBIAN/control" > /tmp/control && mv /tmp/control "${PKG_DIR}/ODR-DabMux-${VERSION}~${DEB_VERSION}_${ARCH}/DEBIAN/control"
+cp -r ${RUN_DIR}/odr-dabmux/DEBIAN/ "${DESTDIR}"
+sed "s/##PACKAGE##/${PKG_NAME}/g" "${DESTDIR}/DEBIAN/control" > /tmp/control && mv /tmp/control "${DESTDIR}/DEBIAN/control"
+sed "s/##VERSION##/${PKG_VERSION}/g" "${DESTDIR}/DEBIAN/control" > /tmp/control && mv /tmp/control "${DESTDIR}/DEBIAN/control"
 cd ${PKG_DIR}/
-dpkg-deb --build ODR-DabMux-${VERSION}~${DEB_VERSION}_${ARCH}
+dpkg-deb --build ${DESTDIR}
